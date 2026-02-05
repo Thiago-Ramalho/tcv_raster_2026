fa4c59601a8a97b13604ad8247ddc6d24436cecc
Thiago-Ramalho <trm4@cin.ufpe.br>
2026-02-05 18:16:44 -0300

    feat(disconnected_triangles_scene.py): add similar scene to two_triangles_scene but the triangles are apart

---
+++ disconnected_triangles_scene.py
@@ -0,0 +1,12 @@
+from src.base import BaseScene, Color
+from src.shapes import Triangle
+
+# class name should be Scene
+class Scene(BaseScene):
+    def __init__(self):
+        super().__init__("Two Triangles Scene")
+        self.background = Color(1, 1, 1)
+
+        # Add some triangles to the scene
+        self.add(Triangle((1.0, 1.0), (3.0, 1.0), (2.0, 3.0)), Color(1.0, 0.0, 0.0))  # Red triangle
+        self.add(Triangle((4.0, 3.0), (5.0, 1.0), (6.0, 4.0)), Color(0.0,0.0,1.0)) # Blue triangle
